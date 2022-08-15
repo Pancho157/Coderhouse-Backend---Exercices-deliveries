8d95d6b3f97da6b7fad78b87da37b61962e8bfa2
Pancho157 <hoxocraft65@gmail.com>
2022-08-15 18:17:36 -0300

    Generadas las rutas en otro archivo

--- Entregables/Entrega 4 (API REST)/index.js
+++ Entregables/Entrega 4 (API REST)/index.js
@@ -1,81 +1,14 @@
-var express = require("express");
-const { Contenedor } = require("./Contenedor");
-
-const products = new Contenedor("./productos.txt");
+const express = require("express");
 
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/productos", async (req, res) => {
-  try {
-    let allProducts = await products.getAll();
-    res.json(allProducts);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Hubo un error al traer los productos");
-  }
-});
-
-app.get("/api/productos:id", async (req, res) => {
-  try {
-    let id = req.params.id;
-    let product = await products.getById(`${id}`);
-
-    if (product == []) {
-      product = { Error: "Producto no encontrado" };
-    }
-    res.json(product);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Hubo un error al traer el producto");
-  }
-});
-
-app.post("/api/productos/", async (req, res) => {
-  try {
-    const { productTitle, productPrice, productThumbnail } = req.body;
-
-    if (!productTitle || !productPrice || !productThumbnail) {
-      res.status(400).send("Tus datos no están completos");
-    }
-
-    let product = await products.save({
-      title: productTitle,
-      price: productPrice,
-      thumbnail: productThumbnail,
-    });
-    //TODO: Modificar la clase contenedor para que devuelva el ID del producto sin ser por la consola
-    res.json(`El ID del producto guardado es: ${product.id}`);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Hubo un error al guardar el producto");
-  }
-});
-
-app.put("/api/productos/:id", async (req, res) => {
-  try {
-    // TODO: Modificar la clase Contenedor para poder actualizar productos
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Hubo un error al guardar el producto");
-  }
-});
-
-app.delete("/api/productos/:id", async (req, res) => {
-  try {
-    let id = req.params.id;
-    await products.deleteById(`${id}`);
-    res.json(`Se ha eliminado el producto con el ID: ${id}`);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Hubo un error al eliminar el producto");
-  }
-});
+app.use("/api/productos", require("./router/productos"));
 
 // Da el error 404, debe estar debajo de todas las rutas
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).send("No se encontró la página que estás buscando");
 });
 
---
+++ Entregables/Entrega 4 (API REST)/router/productos.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { Contenedor } = require("../Contenedor");
+
+const products = new Contenedor("./productos.txt");
+
+
+
+router.get("/", async (req, res) => {
+  try {
+    let allProducts = await products.getAll();
+    res.json(allProducts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error al traer los productos");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    let product = await products.getById(`${id}`);
+
+    if (product == []) {
+      product = { Error: "Producto no encontrado" };
+    }
+    res.json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error al traer el producto");
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { productTitle, productPrice, productThumbnail } = req.body;
+
+    if (!productTitle || !productPrice || !productThumbnail) {
+      res.status(400).send("Tus datos no están completos");
+    }
+
+    let product = await products.save({
+      title: productTitle,
+      price: productPrice,
+      thumbnail: productThumbnail,
+    });
+    //TODO: Modificar la clase contenedor para que devuelva el ID del producto sin ser por la consola
+    res.json(`El ID del producto guardado es: ${product.id}`);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error al guardar el producto");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    // TODO: Modificar la clase Contenedor para poder actualizar productos
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error al guardar el producto");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    await products.deleteById(`${id}`);
+    res.json(`Se ha eliminado el producto con el ID: ${id}`);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error al eliminar el producto");
+  }
+});
+
+module.exports = router;
