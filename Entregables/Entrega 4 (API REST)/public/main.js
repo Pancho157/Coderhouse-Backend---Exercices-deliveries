a7de658fd86ac69dda07c08edbb6cf0612eba4b7
Pancho157 <hoxocraft65@gmail.com>
2022-08-16 00:58:14 -0300

    Arreglado el HTML - Falta arreglar el Fetch del formulario y los metodos GET (con id) y PUT

--- Entregables/Entrega 4 (API REST)/public/main.js
+++ Entregables/Entrega 4 (API REST)/public/main.js
@@ -2,20 +2,22 @@ const postProduct = async () => {
   const title = document.getElementById("title");
   const price = document.getElementById("price");
   const thumbnail = document.getElementById("thumbnail");
+  document.getElementById("form").reset();
 
-  const data = { title, price, thumbnail };
-
+  // TODO: Reparar el fetch para el POST
   try {
     const request = await fetch("/api/productos", {
       method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
+      body: {
+        productTitle: title,
+        productPrice: price,
+        productThumbnail: thumbnail,
+      },
     });
 
     const result = await request.json();
 
     // Deja la data en el formulario si hay un error
-    document.getElementById("form").reset();
 
     return result;
   } catch (err) {
