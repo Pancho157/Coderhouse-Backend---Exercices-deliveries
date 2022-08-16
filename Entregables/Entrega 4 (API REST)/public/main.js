29e399472c975d628ac2cfc6c46bf60878652a65
Pancho157 <hoxocraft65@gmail.com>
2022-08-15 22:33:55 -0300

    Generada la lógica del formulario

---
+++ Entregables/Entrega 4 (API REST)/public/main.js
@@ -0,0 +1,22 @@
+const agregarProducto = async () => {
+  const title = document.getElementById("title");
+  const price = document.getElementById("price");
+  const thumbnail = document.getElementById("thumbnail");
+
+  try {
+    const data = { title, price, thumbnail };
+    const request = await fetch("/api/productos", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    });
+    const result = await request.json();
+
+    // Deja la data en el formulario si hay un error
+    document.getElementById("form").reset();
+
+    return result;
+  } catch (err) {
+    return new Error(err);
+  }
+};
