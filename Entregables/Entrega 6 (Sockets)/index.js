5a133a403ad0d2c614cb7effdff4051e8a8491a6
Pancho157 <hoxocraft65@gmail.com>
2022-08-29 20:55:26 -0300

    Corregidos los readme cruzados

--- Entregables/Entrega 6 (Sockets)/index.js
+++ Entregables/Entrega 6 (Sockets)/index.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const { Server: IOServer } = require("socket.io");
+
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+    console.log(`Server on - Port: ${PORT}`);
+})
