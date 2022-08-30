4e0c82be684a31847143050aa8a1569b2b8942b8
Pancho157 <hoxocraft65@gmail.com>
2022-08-29 22:39:08 -0300

    jugando con sockets

--- Entregables/Entrega 6 (Sockets)/index.js
+++ Entregables/Entrega 6 (Sockets)/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const { Server: HttpServer } = require("http");
 const { Server: IOServer } = require("socket.io");
 
-const app = express()
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+io.on(`connection`, (socket) => {
+  console.log("Nuevo cliente conectado");
+});
+
+
 
 const PORT = process.env.PORT || 8080;
-const server = app.listen(PORT, () => {
-    console.log(`Server on - Port: ${PORT}`);
-})
+const connectedServer = httpServer.listen(PORT, () => {
+  console.log(`Http - Socket Server On - Port: ${PORT}`);
+});
+
+connectedServer.on("error", (error) => console.warn(error));
