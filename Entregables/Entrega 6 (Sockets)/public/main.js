6eda73a84edafc6bc2518974c0accea3dc0a0d49
Pancho157 <hoxocraft65@gmail.com>
2022-09-01 18:38:05 -0300

    finalizado el main.js

--- Entregables/Entrega 6 (Sockets)/public/main.js
+++ Entregables/Entrega 6 (Sockets)/public/main.js
@@ -41,8 +41,10 @@ const renderMessages = (data) => {
               </div>`;
     })
     .join(" ");
-
+  // Renderiza los mensajes
   document.getElementById("chat__messagesContainer").innerHTML = html;
+
+  // Renderiza los productos en la tabla
   const productsHTML = data.products
     .map((product) => {
       return `
@@ -59,26 +61,24 @@ const renderMessages = (data) => {
 };
 
 // -------------------- Guardar producto (formulario) ---------------------
-const guardarProducto = async () => {
+const guardarProducto = () => {
   const form = document.getElementById("form").value;
   const title = document.getElementById("title").value;
   const price = document.getElementById("price").value;
   const thumbnail = document.getElementById("thumbnail").value;
 
-  try {
-    const response = await fetch("/api/productos", {
-      method: "POST",
-      body: JSON.stringify({
-        title: title,
-        price: price,
-        thumbnail: thumbnail,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-    console.log(response);
-    // Deja la data en el formulario si hay un error
-    form.reset();
-  } catch (err) {
-    return new Error(err);
-  }
+  const product = {
+    title: title,
+    price: price,
+    thumbnail: thumbnail,
+  };
+
+  socket.emit("new-product", product);
+  form.reset();
+  return false;
 };
+
+// Recibe los productos del servidor
+socket.on("messagesFromServer", (messages) => {
+  renderMessages(messages);
+});
