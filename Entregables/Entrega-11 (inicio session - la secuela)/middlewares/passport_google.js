6770b6e79dd27da07eb17675fe5abed43a0d818d
Francisco Muñiz <hoxocraft65@gmail.com>
2022-11-04 10:24:29 -0300

    Generated google-passport middleware

---
+++ Entregables/Entrega-11 (inicio session - la secuela)/middlewares/passport_google.js
@@ -0,0 +1,28 @@
+const passport = require("passport");
+const {
+  Strategy: GoogleStrategy,
+  Profile,
+  VerifyCallback,
+} = require("passport-google-oauth20");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.OAUTH_CLI_ID,
+      clientSecret: process.env.OAUTH_CLI_SECRET,
+      callbackURL: process.env.OAUTH_REDIRECT_URL,
+      scope: ["email", "profile"],
+    },
+    async (accessTocken, refreshTocken, profile, done) => {
+      console.log();
+      console.log();
+      console.log(accessTocken);
+      console.log();
+      console.log();
+      console.log(profile);
+      console.log();
+      console.log();
+      console.log(done);
+    }
+  )
+);
